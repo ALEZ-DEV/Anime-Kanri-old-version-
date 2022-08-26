604aba8a084c1936e79480ca0f7ad7be07ea683d
ALEZ <raidersfocus@gmail.com>
2022-08-26 16:21:48 +0200

    file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.FileExplorerBox = new System.Windows.Forms.ListBox();
+            this.fileExplorerBox = new System.Windows.Forms.ListBox();
             this.setting = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // FileExplorerBox
+            // fileExplorerBox
             // 
-            this.FileExplorerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FileExplorerBox.FormattingEnabled = true;
-            this.FileExplorerBox.ItemHeight = 25;
-            this.FileExplorerBox.Location = new System.Drawing.Point(219, 15);
-            this.FileExplorerBox.Name = "FileExplorerBox";
-            this.FileExplorerBox.Size = new System.Drawing.Size(569, 404);
-            this.FileExplorerBox.TabIndex = 0;
-            this.FileExplorerBox.SelectedIndexChanged += new System.EventHandler(this.FileExplorerBox_SelectedIndexChanged);
+            this.fileExplorerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileExplorerBox.FormattingEnabled = true;
+            this.fileExplorerBox.ItemHeight = 20;
+            this.fileExplorerBox.Location = new System.Drawing.Point(219, 15);
+            this.fileExplorerBox.Name = "fileExplorerBox";
+            this.fileExplorerBox.Size = new System.Drawing.Size(569, 404);
+            this.fileExplorerBox.TabIndex = 0;
+            //this.fileExplorerBox.SelectedIndexChanged += new System.EventHandler(this.FileExplorerBox_SelectedIndexChanged);
             // 
             // setting
             // 
@@ -53,23 +54,35 @@
             this.setting.TabIndex = 1;
             this.setting.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.setting);
-            this.Controls.Add(this.FileExplorerBox);
+            this.Controls.Add(this.fileExplorerBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox FileExplorerBox;
+        private System.Windows.Forms.ListBox fileExplorerBox;
         private System.Windows.Forms.Button setting;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,19 +12,63 @@ namespace AnimeManager
 {
     public partial class Form1 : Form
     {
+        List<string> DirList = new List<string>();
+
+        Manager manager = new Manager();
+
+        string defaultPath;
+        string path;
+        List<string> pathHistory = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
 
-            var manager = new Manager();
-            manager.initList();
+            defaultPath = @"C:\Anime";
+            path = defaultPath;
 
-            manager.BindListToListBox(FileExplorerBox, manager.list);
+            DirList = manager.GetAllFolderOrFileName(path);
+
+            fileExplorerBox.DoubleClick += new EventHandler(fileExplorerBoxItem_Click);
+
+            manager.BindListToListBox(fileExplorerBox, DirList, true);
         }
 
-        private void FileExplorerBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void fileExplorerBoxItem_Click(object sender, System.EventArgs e)
         {
 
+            if (fileExplorerBox.SelectedItem.ToString() == "..")
+            {
+                pathHistory.RemoveAt(pathHistory.Count() - 1);
+
+                path = defaultPath;
+
+                foreach (string dir in pathHistory)
+                {
+
+                    path += @"\" + dir;
+
+                }
+
+            } else
+            {
+                path += @"\" + fileExplorerBox.SelectedItem.ToString();
+                pathHistory.Add(fileExplorerBox.SelectedItem.ToString());
+            }
+            
+
+            DirList.Clear();
+
+            DirList = manager.GetAllFolderOrFileName(path);
+
+            if (path == defaultPath)
+            {
+                manager.BindListToListBox(fileExplorerBox, DirList, true);
+            }
+            else
+            {
+                manager.BindListToListBox(fileExplorerBox, DirList, false);
+            }
         }
     }
 }
